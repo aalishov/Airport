8e8340d0bcaacdbc9a85a8b477ff9326fb85825d
Alish Alishov <a.alishov@live.com>
2023-02-03 11:51:21 +0200

    Add model Airport

---
+++ Airport/Airport.Models/Airport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Airport.Models
+{
+    public class Airport
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string AirportName { get; set; }
+
+        [MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
